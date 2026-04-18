39ab6a99b47e744b16bcd56b9ffe71397ec20aac
Muhammed Hussain <hussainmaheen88@gmail.com>
2026-04-18 13:27:41 +0530

    dev : sdk updated

--- sdks/python/setup.py
+++ sdks/python/setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="tokenbee-sdk",
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages(),
     install_requires=["httpx>=0.23.0"],
     author="TokenBee Inc.",
